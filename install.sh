d2626dd641c4fef84b902ac0ea052797f4512ff0
Flo <fdefontis@gmail.com>
2024-06-29 20:47:47 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Running installation script"
-# Add your script's commands here
 
+# Run like this:
+# wget -qO- https://raw.githubusercontent.com/flo8/debian/master/install.sh | bash
+
+# Add your script's commands here
 apt-get update
 apt-get upgrade
 
