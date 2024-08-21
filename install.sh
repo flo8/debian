5b3f2d6efaba34e78c9438f1b3c655f4cd134e27
Flo <fdefontis@gmail.com>
2024-08-21 18:24:29 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,19 +37,25 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/clickhouse-keyring.gpg] http
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y clickhouse-server clickhouse-client clickhouse-common-static
 
-# Install ACL tool
-sudo apt-get install -y acl
-
 # Update clickhouse server conf file and permissions
 sudo sed -i 's|<user_files_path>/var/lib/clickhouse/user_files/</user_files_path>|<user_files_path>/home/debian/apps</user_files_path>|g' /etc/clickhouse-server/config.xml
 sudo chown -R clickhouse /var/lib/clickhouse /var/log/clickhouse-server /etc/clickhouse-server /etc/clickhouse-client
 mkdir /home/debian/apps
 mkdir /home/debian/agent
-sudo setfacl -R -m u:clickhouse:rx /home/debian/apps
-sudo setfacl -R -m d:u:clickhouse:rx /home/debian/apps
 
+# Set read/execute permission for everyone on this folder
+sudo chmod -R o+x /home/debian/apps
+
+# This can be used to check later on if it worked
+# sudo -u clickhouse ls ./apps/8muolqjjt3cwsnjp/v5
+
+# This finally didn't work, removing it!
+# Install ACL tool
+# sudo apt-get install -y acl
+# sudo setfacl -R -m u:clickhouse:rx /home/debian/apps
+# sudo setfacl -R -m d:u:clickhouse:rx /home/debian/apps
 # Set default ACL for the directory and all future subdirectories and files
-sudo setfacl -m default:u:clickhouse:rx /home/debian/apps
+# sudo setfacl -m default:u:clickhouse:rx /home/debian/apps
 
 # Make sure ClickHouse server is not started automatically after every reboot.
 sudo systemctl enable clickhouse-server.service
