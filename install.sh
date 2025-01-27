f9dd186afa5c2cbfc60b66c433600080a4fbaacc
Flo <fdefontis@gmail.com>
2025-01-27 11:03:37 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,34 +41,6 @@ source ~/.bashrc
 sudo timedatectl set-ntp true
 timedatectl status
 
-# Adds fail2ban configuration
-sudo wget -O /etc/fail2ban/jail.local https://raw.githubusercontent.com/flo8/debian/main/jail.local
-sudo systemctl start fail2ban
-sudo systemctl enable fail2ban
-
-# Display status of fail2ban
-sudo fail2ban-client status
-sudo fail2ban-client status sshd
-
-# Adds UFW and configure it
-wget -qO- https://raw.githubusercontent.com/flo8/debian/main/ufw.sh | bash
-
-# Set correct permission and default security values
-# Note that LogLevel is MANDATORY for fail2ban
-cat <<EOF | sudo tee -a /etc/ssh/sshd_config
-LogLevel INFO
-LoginGraceTime 1m
-PermitRootLogin prohibit-password
-StrictModes yes
-MaxAuthTries 3
-MaxSessions 5
-PubkeyAuthentication yes
-PermitEmptyPasswords no
-EOF
-
-# Check SSH is correct
-sudo sshd -t
-
 # Everything installed
 echo "Press Prefix + [I] to install tpm in tmux"
 
