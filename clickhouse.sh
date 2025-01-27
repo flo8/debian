eee03d306871672d94900b41489c173cf1eb7d69
Flo <fdefontis@gmail.com>
2025-01-27 09:55:18 +0900

    Update clickhouse.sh

--- clickhouse.sh
+++ clickhouse.sh
@@ -19,3 +19,18 @@ sudo chmod -R o+x /home/debian/apps
 
 # This can be used to check later on if it worked
 # sudo -u clickhouse ls ./apps/8muolqjjt3cwsnjp/v5
+
+# This finally didn't work, removing it!
+# Install ACL tool
+# sudo apt-get install -y acl
+# sudo setfacl -R -m u:clickhouse:rx /home/debian/apps
+# sudo setfacl -R -m d:u:clickhouse:rx /home/debian/apps
+# Set default ACL for the directory and all future subdirectories and files
+# sudo setfacl -m default:u:clickhouse:rx /home/debian/apps
+
+# Make sure ClickHouse server is not started automatically after every reboot.
+sudo systemctl enable clickhouse-server.service
+sudo systemctl daemon-reload service
+
+# Restart Clickhouse server
+sudo service clickhouse-server restart
