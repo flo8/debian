1d4fdab0357cea2883e789e0f5fc23de04fff857
flo8 <flo@air360.io>
2026-05-21 09:07:29 +0900

    chore: version bump

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ set -euo pipefail
 # ============================================================================
 
 # ========= CONFIG =========
-VERSION="1.1.10"
+VERSION="1.1.11"
 USERNAME="flo"
 PUBKEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEjGiJLi9DlEA8h0GKTz9WtvD6P2XE9C/KHn5nKtKC2Y flo@lothlorien"
 REPO_RAW="https://raw.githubusercontent.com/flo8/debian/main"
